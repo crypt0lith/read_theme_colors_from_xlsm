e09001bdb8219a675f07a763b338b7de090d8236
crypt0lith <118923461+crypt0lith@users.noreply.github.com>
2022-11-24 17:08:57 -0600

    Update read theme colors from excel file.py

--- read theme colors from excel file.py
+++ read theme colors from excel file.py
@@ -4,18 +4,19 @@ from openpyxl import load_workbook
 data_file = 'data/exampleFile.xlsm'
 
 wb = load_workbook(data_file)
-xlmns = 'http://schemas.openxmlformats.org/drawingml/2006/main'
+xmlns = 'http://schemas.openxmlformats.org/drawingml/2006/main'
 root = fromstring(wb.loaded_theme)
-themeEl = root.find(QName(xlmns, 'themeElements').text)
-colorSchemes = themeEl.findall(QName(xlmns, 'clrScheme').text)
+themeEl = root.find(QName(xmlns, 'themeElements').text)
+colorSchemes = themeEl.findall(QName(xmlns, 'clrScheme').text)
 firstColorScheme = colorSchemes[0]
 
 colors = []
 for c in ['lt1', 'dk1', 'lt2', 'dk2', 'accent1', 'accent2', 'accent3', 'accent4', 'accent5', 'accent6']:
-    accent = firstColorScheme.find(QName(xlmns, c).text)
+    accent = firstColorScheme.find(QName(xmlns, c).text)
 
     if 'window' in accent.getchildren()[0].attrib['val']:
         colors.append(accent.getchildren()[0].attrib['lastClr'])
     else:
         colors.append(accent.getchildren()[0].attrib['val'])
-print(colors)
+
+    print(c, str(colors.pop()))
